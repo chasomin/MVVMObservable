6b7deddfadaee883b9e5890ba58657109719ed08
chasomin <somin49@naver.com>
2024-02-22 16:05:19 +0900

    Initial Commit

---
+++ MVVMObservable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMObservable
+//
+//  Created by 차소민 on 2/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
